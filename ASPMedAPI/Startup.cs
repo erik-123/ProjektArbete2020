999d5ba7032da24921d758865ef36242e406f6ad
erik-123 <erik.sondell@outlook.com>
2019-12-31 11:49:07 +0100

    Add project files.

---
+++ ASPMedAPI/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ASPMedAPI.Startup))]
+namespace ASPMedAPI
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
