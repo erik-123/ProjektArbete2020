9cf86a5ca7c3f65eb90f1f1afc9b878b2bbf4fcb
Hugosterberg <59054135+Hugosterberg@users.noreply.github.com>
2020-01-08 12:39:33 +0100

    ändringar

--- ASPMedAPI/Controllers/HomeController.cs
+++ ASPMedAPI/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ASPMedAPI.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,6 +9,7 @@ namespace ASPMedAPI.Controllers
 {
     public class HomeController : Controller
     {
+
         public ActionResult Index()
         {
             return View();
